76f3bc88d38a7726f9f602dd9b5bcfcca85f1968
Fredrik Bagge Carlson <cont-frb@ulund.org>
2019-04-09 07:48:23 +0200

    move tests to runtests.jl

--- src/MonteCarloMeasurements.jl
+++ src/MonteCarloMeasurements.jl
@@ -116,32 +116,3 @@ end
 # module Operators
 # end
 end # module
-
-
-
-
-using Main.MonteCarloMeasurements
-using Test
-f(x) = 2x + 10
-p = Particles(10000)
-@test 9.96 < mean(f(p)) < 10.04
-@test 9.96 < f(p) < 10.04
-@test f(p) ≈ 10
-@test !(f(p) ≲ 11)
-@test f(p) ≲ 15
-@test 5 ≲ f(p)
-Normal(f(p)).μ ≈ mean(f(p))
-
-# plot(sin(0.1p)*sin(0.1p))
-
-A = [Particles(1000) for i = 1:3, j = 1:3]
-a = [Particles(1000) for i = 1:3]
-b = [Particles(1000) for i = 1:3]
-@test sum(a.*b) ≈ 0
-@test all(A*b .≈ [0,0,0])
-
-
-
-using ControlSystems
-p = 1 + 0.1Particles(100)
-G = tf([p], [1, 2, 1])
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
 using MonteCarloMeasurements
 using Test
 
-@testset "MonteCarloMeasurements.jl" begin
-    # Write your own tests here.
-end
+# @testset "MonteCarloMeasurements.jl" begin
+
+f(x) = 2x + 10
+p = Particles(10000)
+@test 9.96 < mean(f(p)) < 10.04
+@test 9.96 < f(p) < 10.04
+@test f(p) ≈ 10
+@test !(f(p) ≲ 11)
+@test f(p) ≲ 15
+@test 5 ≲ f(p)
+Normal(f(p)).μ ≈ mean(f(p))
+
+# plot(sin(0.1p)*sin(0.1p))
+
+A = [Particles(1000) for i = 1:3, j = 1:3]
+a = [Particles(1000) for i = 1:3]
+b = [Particles(1000) for i = 1:3]
+@test sum(a.*b) ≈ 0
+@test all(A*b .≈ [0,0,0])
+
+
+
+using ControlSystems
+p = 1 + 0.1Particles(100)
+G = tf([p], [1, 2, 1])
+
+# end
