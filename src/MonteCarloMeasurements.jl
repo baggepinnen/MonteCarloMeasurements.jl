32ffb18deff45ceea7bf1308c7bd443fef510fac
Fredrik Bagge Carlson <cont-frb@ulund.org>
2019-04-22 12:29:18 +0200

    remove reexport, manually export some Distr.

--- src/MonteCarloMeasurements.jl
+++ src/MonteCarloMeasurements.jl
@@ -4,8 +4,14 @@ const DEFAUL_NUM_PARTICLES = 500
 const DEFAUL_STATIC_NUM_PARTICLES = 100
 
 export Particles,StaticParticles, WeightedParticles, ≲,≳, systematic_sample, outer_product, meanstd, meanvar, ℝⁿ2ℝⁿ_function, ℂ2ℂ_function, resample!
-export mean, std, cov, var, quantile, median
+# Plot exports
 export errorbarplot, mcplot, ribbonplot
+
+# Statistics reexport
+export mean, std, cov, var, quantile, median
+# Distributions reexport
+export Normal, MvNormal, Cauchy, Beta, Exponential, Gamma, Laplace, Uniform, fit, logpdf
+
 
 import Base: add_sum
 
