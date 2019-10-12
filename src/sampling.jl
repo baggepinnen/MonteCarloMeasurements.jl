7a3fe4ee4b729d5267d6478fdca66b24a080f66d
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-10-13 07:42:47 +0800

    Correct systematic sample to always have perfect mean

--- src/sampling.jl
+++ src/sampling.jl
@@ -3,7 +3,7 @@
 returns a `Vector` of length `N` sampled systematically from the distribution `d`. If `permute=false`, this vector will be sorted.
 """
 function systematic_sample(N, d=Normal(0,1); permute=true)
-    e   = rand()/N
+    e   = 0.5/N # rand()/N
     y   = e:1/N:1
     o = map(y) do y
         quantile(d,y)
