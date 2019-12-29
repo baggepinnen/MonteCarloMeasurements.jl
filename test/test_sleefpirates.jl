efa71b631641da5ceded07719c00aec5d6803550
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-12-29 14:10:15 +0800

    test sleefpirates

---
+++ test/test_sleefpirates.jl
@@ -0,0 +1,8 @@
+using SLEEFPirates
+a = rand(500)
+@test map(exp, a) ≈ exp(Particles(a)).particles
+@test map(log, a) ≈ log(Particles(a)).particles
+
+a = rand(Float32, 500)
+@test map(exp, a) ≈ exp(Particles(a)).particles
+@test map(log, a) ≈ log(Particles(a)).particles
