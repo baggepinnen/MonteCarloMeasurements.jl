1631da307de1afd0f6bf231fe617510a8b04cc6b
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-12-29 15:15:07 +0800

    overload more functions

--- src/sleefpirates.jl
+++ src/sleefpirates.jl
@@ -8,20 +8,29 @@ for ff in (exp,)
                 res = map((SLEEFPirates.$f), p.particles)
                 return $PT{Float64,N}(res)
             end
+            function ($m.$f)(p::$PT{Float32,N}) where {Float32,N}
+                res = map((SLEEFPirates.$f), p.particles)
+                return $PT{Float32,N}(res)
+            end
         end)
     end
 end
 
 
-for ff in (exp,log)
+for ff in (log, sin, cos, tan, asin, acos, atan)
     f = nameof(ff)
+    fs = Symbol(f,"_fast")
     m = Base.parentmodule(ff)
     for PT in (:Particles, :StaticParticles)
         eval(quote
-            function ($m.$f)(p::$PT{Float32,N}) where {Float32,N}
-                res = map((SLEEFPirates.$f), p.particles)
-                return $PT{Float32,N}(res)
-            end
+        function ($m.$f)(p::$PT{Float64,N}) where {Float64,N}
+            res = map((SLEEFPirates.$fs), p.particles)
+            return $PT{Float64,N}(res)
+        end
+        function ($m.$f)(p::$PT{Float32,N}) where {Float32,N}
+            res = map((SLEEFPirates.$fs), p.particles)
+            return $PT{Float32,N}(res)
+        end
         end)
     end
 end
