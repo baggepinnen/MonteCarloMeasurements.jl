f321ebca1865295f6fa8bd91127b50e9f90559cf
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-12-29 19:26:26 +0800

    test more sleef

--- test/test_sleefpirates.jl
+++ test/test_sleefpirates.jl
@@ -2,7 +2,10 @@ using SLEEFPirates
 a = rand(500)
 @test map(exp, a) ≈ exp(Particles(a)).particles
 @test map(log, a) ≈ log(Particles(a)).particles
+@test map(cos, a) ≈ cos(Particles(a)).particles
+
 
 a = rand(Float32, 500)
 @test map(exp, a) ≈ exp(Particles(a)).particles
 @test map(log, a) ≈ log(Particles(a)).particles
+@test map(cos, a) ≈ cos(Particles(a)).particles
