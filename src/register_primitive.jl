76ccacd1e6569b98b121dee5417320eb1c8cb82e
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-01-03 19:19:00 +0800

    remove binop

--- src/register_primitive.jl
+++ src/register_primitive.jl
@@ -81,31 +81,6 @@ function register_primitive_multi(ff, eval=eval)
     end)
 end
 
-function register_primitive_binop(ff, eval=eval)
-    f = nameof(ff)
-    m = Base.parentmodule(ff)
-    for PT in (:Particles, :StaticParticles)
-        eval(quote
-            function ($m.$f)(p::$PT{T,N},a::Real...) where {T,N}
-                res = ($m.$f).(p.particles, MonteCarloMeasurements.maybe_particles.(a)...) # maybe_particles introduced to handle >2 arg operators
-                return $PT{eltype(res),N}(res)
-            end
-            function ($m.$f)(a::Real,p::$PT{T,N}) where {T,N}
-                res = ($m.$f).(a, p.particles)
-                return $PT{eltype(res),N}(res)
-            end
-            function ($m.$f)(p1::$PT{T,N},p2::$PT{T,N}) where {T,N}
-                res = ($m.$f).(p1.particles, p2.particles)
-                return $PT{eltype(res),N}(res)
-            end
-            function ($m.$f)(p1::$PT{T,N},p2::$PT{S,N}) where {T,S,N} # Needed for particles of different float types :/
-                res = ($m.$f).(p1.particles, p2.particles)
-                return $PT{eltype(res),N}(res)
-            end
-        end)
-    end
-end
-
 """
     register_primitive_single(ff, eval=eval)
 
