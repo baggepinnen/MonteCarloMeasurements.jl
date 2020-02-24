40af498b1e5c6abef499686fdf7e311431420975
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-02-24 08:15:17 +0800

    reduce particle size in sleefpirates test

--- test/test_sleefpirates.jl
+++ test/test_sleefpirates.jl
@@ -2,7 +2,7 @@
     @info "Testing SLEEFPirates"
 
     using SLEEFPirates
-    a = rand(500)
+    a = rand(100)
     @test map(exp, a) ≈ exp(Particles(a)).particles
     @test map(log, a) ≈ log(Particles(a)).particles
     @test map(cos, a) ≈ cos(Particles(a)).particles
@@ -12,7 +12,7 @@
     @test map(cos, a) ≈ cos(StaticParticles(a)).particles
 
 
-    a = rand(Float32, 500)
+    a = rand(Float32, 100)
     @test map(exp, a) ≈ exp(Particles(a)).particles
     @test map(log, a) ≈ log(Particles(a)).particles
     @test map(cos, a) ≈ cos(Particles(a)).particles
