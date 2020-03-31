1f6266da6117c155e97acca203f9bd039311b20f
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-03-31 16:04:44 +0800

    change to @eval

--- src/sleefpirates.jl
+++ src/sleefpirates.jl
@@ -3,7 +3,7 @@ for ff in (exp,)
     f = nameof(ff)
     m = Base.parentmodule(ff)
     for PT in (:Particles, :StaticParticles)
-        eval(quote
+        @eval begin
             @inline function ($m.$f)(p::$PT{Float64,N}) where N
                 res = (SLEEFPirates.$f).(p.particles)
                 return $PT{Float64,N}(res)
@@ -12,7 +12,7 @@ for ff in (exp,)
                 res = (SLEEFPirates.$f).(p.particles)
                 return $PT{Float32,N}(res)
             end
-        end)
+        end
     end
 end
 
@@ -22,15 +22,15 @@ for ff in (log, sin, cos, asin, acos, atan) # tan is not faster
     fs = Symbol(f,"_fast")
     m = Base.parentmodule(ff)
     for PT in (:Particles, :StaticParticles)
-        eval(quote
-        @inline function ($m.$f)(p::$PT{Float64,N}) where N
-            res = (SLEEFPirates.$fs).(p.particles)
-            return $PT{Float64,N}(res)
+        @eval begin
+            @inline function ($m.$f)(p::$PT{Float64,N}) where N
+                res = (SLEEFPirates.$fs).(p.particles)
+                return $PT{Float64,N}(res)
+            end
+            @inline function ($m.$f)(p::$PT{Float32,N}) where N
+                res = (SLEEFPirates.$fs).(p.particles)
+                return $PT{Float32,N}(res)
+            end
         end
-        @inline function ($m.$f)(p::$PT{Float32,N}) where N
-            res = (SLEEFPirates.$fs).(p.particles)
-            return $PT{Float32,N}(res)
-        end
-        end)
     end
 end
