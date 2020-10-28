3e3a2d27fe65b72e975f66ead266f28af0ed6cae
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-10-28 05:53:32 +0100

    update docstring

--- src/bymap.jl
+++ src/bymap.jl
@@ -109,7 +109,7 @@ end
 
 
 """
-    prob(ex)
+    @prob a < b
 
 Calculate the probability that an event on any of the forms `a < b, a > b, a <= b, a >= b` occurs, where `a` and/or `b` are of type `AbstractParticles`.
 """
